c718bc70113214d8be655f3e93d14a5c1efa79fe
nekoposer <zimanov.mm@mail.ru>
2023-11-02 13:40:50 +0100

    Project is created

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Order order = new Order();
+        System.out.println(order);
+
+        System.out.println("Hello!");
+    }
+}
